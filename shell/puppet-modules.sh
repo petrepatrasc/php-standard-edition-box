e7f16f0fb5cf17f2b9daa81032c56e8d302c9eba
mariusa <marius.anghel@cegeka.com>
2014-04-17 16:01:24 +0300

    adding ffmpeg latest vaersion

--- shell/puppet-modules.sh
+++ shell/puppet-modules.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sudo apt-get --purge autoremove puppet puppet-common puppetmaster puppetmaster-common -y;
 cd /tmp;
+#wget http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz -O ffmpeg.tar.gz;
+wget http://ffmpeg.gusari.org/static/32bit/ffmpeg.static.32bit.latest.tar.gz -O ffmpeg.tar.gz;
+sudo mv ffmpeg /usr/bin
+sudo mv ffprobe /usr/bin
+sudo apt-get --purge autoremove puppet puppet-common puppetmaster puppetmaster-common -y;
 wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb;
 sudo dpkg -i puppetlabs-release-precise.deb;
 sudo apt-get update;
