a9bacfc39f983f95a8ab997b44a59b32d5796792
mariusa <marius.anghel@cegeka.com>
2014-04-17 15:25:46 +0300

    created box for ubuntu 12.04; php 5.3; apache 2.2; MySql 5.5

--- shell/puppet-modules.sh
+++ shell/puppet-modules.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+sudo apt-get --purge autoremove puppet puppet-common puppetmaster puppetmaster-common -y;
+cd /tmp;
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb;
+sudo dpkg -i puppetlabs-release-precise.deb;
+sudo apt-get update;
+sudo apt-get install puppetmaster puppet -y;
 mkdir -p /etc/puppet/modules;
 puppet module install tPl0ch-composer --force;
 puppet module install nodes/php --force;
