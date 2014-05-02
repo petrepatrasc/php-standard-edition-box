3118362d5fa067b9d5e65373ec76214245a45434
Petre Patrasc <petre@dreamlabs.ro>
2014-05-02 12:59:50 +0300

    Added some missing comments

--- shell/puppet-modules.sh
+++ shell/puppet-modules.sh
@@ -1,24 +1,25 @@
 <<<<<<< HEAD
 #!/bin/sh
 cd /tmp;
-#wget http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz -O ffmpeg.tar.gz;
+
+# FFmpeg + FFprobe
+#wget http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz -O ffmpeg.tar.gz; - 64bit link, if required.
 wget http://ffmpeg.gusari.org/static/32bit/ffmpeg.static.32bit.latest.tar.gz -O ffmpeg.tar.gz;
 tar -xzvf ffmpeg.tar.gz;
 sudo mv ffmpeg /usr/bin
 sudo mv ffprobe /usr/bin
+
+# Get latest Puppet - not supported by default on Ubuntu 12.04.
 sudo apt-get --purge autoremove puppet puppet-common puppetmaster puppetmaster-common -y;
 wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb;
 sudo dpkg -i puppetlabs-release-precise.deb;
 sudo apt-get update;
 sudo apt-get install puppetmaster puppet -y;
-=======
-# Puppet modules
 
->>>>>>> Added Heroku to list of dependencies
+# Puppet modules
 mkdir -p /etc/puppet/modules;
 puppet module install tPl0ch-composer --force;
 puppet module install nodes/php --force;
 
 # HEROKU
-
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
