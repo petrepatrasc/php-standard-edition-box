705eb61b84d62d0bd3f19623c5fafc5a3054c864
Petre Patrasc <petre@dreamlabs.ro>
2014-05-02 12:20:06 +0300

    Added Heroku to list of dependencies

--- shell/puppet-modules.sh
+++ shell/puppet-modules.sh
@@ -1,6 +1,12 @@
+# Puppet modules
 mkdir -p /etc/puppet/modules;
 puppet module install tPl0ch-composer --force;
 puppet module install nodes/php --force;
+
+# SOLR
 puppet module install puppetlabs-stdlib --force;
 puppet module install example42-puppi --force;
 puppet module install example42-solr --force;
+
+# HEROKU
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
