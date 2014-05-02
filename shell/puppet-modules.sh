bf8fa11e5bc4affff9409512b27be0c65d8a419c
Petre Patrasc <petre@dreamlabs.ro>
2014-05-02 12:20:06 +0300

    Added Heroku to list of dependencies

--- shell/puppet-modules.sh
+++ shell/puppet-modules.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/sh
 cd /tmp;
 #wget http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz -O ffmpeg.tar.gz;
@@ -10,6 +11,14 @@ wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb;
 sudo dpkg -i puppetlabs-release-precise.deb;
 sudo apt-get update;
 sudo apt-get install puppetmaster puppet -y;
+=======
+# Puppet modules
+
+>>>>>>> Added Heroku to list of dependencies
 mkdir -p /etc/puppet/modules;
 puppet module install tPl0ch-composer --force;
 puppet module install nodes/php --force;
+
+# HEROKU
+
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
